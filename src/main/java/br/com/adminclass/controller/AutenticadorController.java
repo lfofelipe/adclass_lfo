d99b2f8791413a1e2bce035651dc5ec4572d9d1f
luis felipe <pro__felipe@hotmail.com>
2015-12-08 23:59:04 -0200

    +funcionalidades

---
+++ src/main/java/br/com/adminclass/controller/AutenticadorController.java
@@ -0,0 +1,39 @@
+package br.com.adminclass.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.admclass.persistencia.entidade.Usuario;
+import br.com.admclass.persistencia.jdbc.UsuarioDAO;
+
+
+public class AutenticadorController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String login = req.getParameter("login");
+		String senha = req.getParameter("senha");
+		Usuario usuario = new Usuario();
+
+		usuario.setLogin(login);
+		usuario.setSenha(senha);
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		Usuario usuAutenticado = usuDAO.Autenticar(usuario);
+		
+		if (usuAutenticado!=null){
+			HttpSession sessao = req.getSession();
+			sessao.setAttribute("usuAutenticado", usuAutenticado);
+			sessao.setMaxInactiveInterval(60*10);
+			
+			req.getRequestDispatcher("WEB-INF/index.jsp").forward(req, resp);
+		}else {
+			resp.getWriter().println("<script>window.alert('não encontrado!'); location.href='login.html';</script>");
+		}
+			
+	}
+
+}
